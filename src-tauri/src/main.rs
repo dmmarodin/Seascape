8b80196cea6f576838c12fc2985c086d97afd335
Douglas Marodin <douglasmarodin@gmail.com>
2022-07-16 23:25:34 -0300

    Add window transparency and shadow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,16 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+  .setup(|app| {
+    let window = app.get_window("main").unwrap();
+    window_vibrancy::apply_acrylic(&window, Some((18,12,24, 150))).unwrap();
+    window_shadows::set_shadow(&window, true).unwrap();
+    Ok(())
+  })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
